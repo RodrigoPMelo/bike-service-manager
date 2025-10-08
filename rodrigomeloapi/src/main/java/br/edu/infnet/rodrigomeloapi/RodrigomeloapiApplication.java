c46e6644cd191baa6c39fb97d1ff3cc9195c65c6
RodrigoPMelo <rodrigo.pomper@hotmail.com>
2025-10-07 22:50:27 -0300

    Adicionado primeira versao do projeto com spring web

---
+++ rodrigomeloapi/src/main/java/br/edu/infnet/rodrigomeloapi/RodrigomeloapiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.rodrigomeloapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RodrigomeloapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RodrigomeloapiApplication.class, args);
+	}
+
+}
---
+++ rodrigomeloapi/src/test/java/br/edu/infnet/rodrigomeloapi/RodrigomeloapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.rodrigomeloapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RodrigomeloapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
