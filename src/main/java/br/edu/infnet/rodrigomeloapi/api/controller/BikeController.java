2cbab98901b61e81975e41d0f4ea70e98c53a52b
RodrigoPMelo <rodrigo.pomper@hotmail.com>
2025-10-13 19:02:51 -0300

    some change

--- src/main/java/br/edu/infnet/rodrigomeloapi/api/controller/BikeController.java
+++ src/main/java/br/edu/infnet/rodrigomeloapi/api/controller/BikeController.java
@@ -1,5 +1,23 @@
 package br.edu.infnet.rodrigomeloapi.api.controller;
 
+import br.edu.infnet.rodrigomeloapi.application.service.BikeService;
+import br.edu.infnet.rodrigomeloapi.domain.model.Bike;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/bikes")
+@RequiredArgsConstructor
 public class BikeController {
 
+    private final BikeService bikeService;
+
+    @GetMapping
+    public List<Bike> listAll() {
+        return bikeService.findAll();
+    }
 }
