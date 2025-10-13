f3f63b372b00b0a092962044607cef501e3efb1e
RodrigoPMelo <rodrigo.pomper@hotmail.com>
2025-10-13 18:18:09 -0300

    feat(domain): add BikeRepositoryPort interface (repository port)

---
+++ src/main/java/br/edu/infnet/rodrigomeloapi/domain/repository/BikeRepositoryPort.java
@@ -0,0 +1,12 @@
+package br.edu.infnet.rodrigomeloapi.domain.repository;
+
+import br.edu.infnet.rodrigomeloapi.domain.model.Bike;
+import java.util.List;
+import java.util.Optional;
+
+public interface BikeRepositoryPort {
+    Bike save(Bike bicicleta);           
+    Optional<Bike> findById(Long id);
+    List<Bike> findAll();
+    void deleteById(Long id);
+}
